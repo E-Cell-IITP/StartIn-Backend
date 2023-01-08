e1d69efd507456d9432e933278ccac90d859b8db
Saurabh Kandpal <saurabh_2001ee63@iitp.ac.in>
2023-01-08 17:54:58 +0530

    invalid username

--- services/users.services.js
+++ services/users.services.js
@@ -8,22 +8,22 @@ const auth = require('../middlewares/auth.js');
 async function login({ username, password }, callback) {
     const user = await User.findOne({ username });
 
-    if(user) {
-        if(bcryptjs.compareSync(password, user.password)) {
+    if (user!=null) {
+        if (bcryptjs.compareSync(password, user.password)) {
             const token = auth.generateAccessToken(username);
-            return callback(null, {...user.toJSON(), token});
-    }
+            return callback(null, { ...user.toJSON(), token });
+        }
     }
     else {
-        return callback ({
+        return callback({
             message: 'Invalid username or password',
-        })   
+        })
     }
 }
 
 //user register
 async function register(params, callback) {
-    if ( params.username === undefined) {
+    if (params.username === undefined) {
         return callback({
             message: 'Username is required',
         });
@@ -31,12 +31,12 @@ async function register(params, callback) {
 
     const user = new User(params);
     user.save()
-    .then((response) => {
-        return callback(null, response);
-    })
-    .catch((error) => {
-        return callback(error);
-    });
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
 }
 
 // async function forgetPassword({Email}, callback ) {
@@ -51,35 +51,35 @@ async function register(params, callback) {
 async function createUserProfit(params, callback) {
     const userProfit = new UserProfit(params);
     userProfit.save()
-    .then((response) => {
-        return callback(null, response);
-    })
-    .catch((error) => {
-        return callback(error);
-    });
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
 }
 
 //team register
 async function teamRegister(params, callback) {
-    if ( params.teamName === undefined) {
+    if (params.teamName === undefined) {
         return callback({
             message: 'Team Name is required',
         });
     }
     const team = new Team(params);
     team.save()
-    .then((response) => {
-        return callback(null, response);
-    })
-    .catch((error) => {
-        return callback(error);
-    });
+        .then((response) => {
+            return callback(null, response);
+        })
+        .catch((error) => {
+            return callback(error);
+        });
 }
 
 // find user in team by username 
 async function findUserInTeam(params, callback) {
-    const user = await Team.find({members : params.username});
-    if ( user != null) {
+    const user = await Team.find({ members: params.username });
+    if (user != null) {
         return callback(null, user);
     }
 }
